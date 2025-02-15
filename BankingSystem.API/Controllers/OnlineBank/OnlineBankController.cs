a755cd78918829ca61ab874463c3e601846483f3
TatiaSalukvadze <tatiasaluqvadze86@gmail.com>
2025-02-15 16:21:30 +0400

    simple change in personService

--- BankingSystem.API/Controllers/OnlineBank/OnlineBankController.cs
+++ BankingSystem.API/Controllers/OnlineBank/OnlineBankController.cs
@@ -1,15 +1,26 @@
 ﻿using BankingSystem.Contracts.DTOs;
+using BankingSystem.Contracts.Interfaces.IServices;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BankingSystem.API.Controllers.OnlineBank
 {
     public class OnlineBankController : WrapperController
     {
+        private readonly IPersonService _personService;
+
+        public OnlineBankController(IPersonService personService)
+        {
+            _personService = personService;
+        }
         [HttpPost]  
         public async Task<IActionResult> RegisterPerson([FromForm] RegisterDTO registerDto)
         {
-            //var (success, Message, data) = await _personService.RegisterPersonAsync(registerDto);
-            return Ok();
+            var (success, Message, data) = await _personService.RegisterPersonAsync(registerDto);
+            if (!success) {
+                return BadRequest(Message);
+            }
+            return Ok(new {Message, data});
         }
     }
 }
--- BankingSystem.Application/Services/PersonService.cs
+++ BankingSystem.Application/Services/PersonService.cs
@@ -2,6 +2,7 @@
 using BankingSystem.Contracts.Interfaces;
 using BankingSystem.Contracts.Interfaces.IServices;
 using BankingSystem.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,15 +15,29 @@ namespace BankingSystem.Application.Services
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IAuthService _authService;
-        public PersonService(IUnitOfWork unitOfWork,IAuthService authService) {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        public PersonService(IUnitOfWork unitOfWork,IAuthService authService, UserManager<IdentityUser> userManager) {
             _unitOfWork = unitOfWork;
             _authService = authService;
+
+            _userManager = userManager;
         }
         public async Task<(bool Success, string Message, object? Data)> RegisterPersonAsync(RegisterDTO registerDto)
         {
-            return (false, "fsdf", null);
             try
             {
+                var existingUser = await _userManager.FindByEmailAsync(registerDto.Email);
+                if (existingUser != null)
+                {
+                    return (false,"A user with this email already exists!",null);
+                }
+                var user = new IdentityUser
+                {
+                    UserName = registerDto.Email,
+                    Email = registerDto.Email,
+                };
+
                 var person = new Person
                 {
                     UserId = "",
