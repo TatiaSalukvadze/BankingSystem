38ebe9991a063b87215e0fff57f6b437606041d8
TatiaSalukvadze <tatiasaluqvadze86@gmail.com>
2025-03-23 17:11:59 +0400

    unit tests

--- BankingSystem.UnitTests/TransactionDetailsServiceTests.cs
+++ BankingSystem.UnitTests/TransactionDetailsServiceTests.cs
@@ -1,12 +1,23 @@
 ﻿using BankingSystem.Application.Services;
 using BankingSystem.Contracts.DTOs.ATM;
 using BankingSystem.Contracts.DTOs.Report;
+using BankingSystem.Contracts.DTOs.UserBanking;
+using BankingSystem.Contracts.DTOs.ATM;
+using BankingSystem.Contracts.DTOs.Report;
 using BankingSystem.Contracts.Interfaces;
 using BankingSystem.Contracts.Interfaces.IExternalServices;
 using BankingSystem.Contracts.Interfaces.IRepositories;
+using BankingSystem.Contracts.Interfaces.IServices;
+using BankingSystem.Domain.Entities;
+using BankingSystem.Domain.Enums;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Features;
 using BankingSystem.Domain.Enums;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
 using Moq;
+using System.Collections.Generic;
+using System.Data;
+using System.Net.NetworkInformation;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BankingSystem.UnitTests
@@ -83,6 +94,193 @@ namespace BankingSystem.UnitTests
             _mockUnitOfWork.Verify(u => u.TransactionDetailsRepository.AverageBankProfitAsync(), Times.Once);
         }
 
+
+        [Fact]
+        public async Task CreateTransactionAsync_ShouldCreate()
+        {
+            var bankProfit = 1;
+            var amount = 10;
+            var fromAccountId = 1;
+            var toAccountId = 2;
+            var currency = "GEL";
+            var isATM = false;
+         
+            _mockUnitOfWork.Setup(u => u.TransactionDetailsRepository.CreateTransactionAsync(It.IsAny<TransactionDetails>())).ReturnsAsync(1);
+
+            var response = await _transactionDetailsService.CreateTransactionAsync(bankProfit, amount, fromAccountId, toAccountId, currency);
+
+            Assert.True(response.Success);
+            Assert.Equal("Transaction was successfull!", response.Message);
+            Assert.Equal(200, response.StatusCode);
+
+            _mockUnitOfWork.Verify(u => u.SaveChanges(), Times.Once());
+            _mockUnitOfWork.Verify(u => u.TransactionDetailsRepository, Times.Exactly(2));
+        }
+
+        [Fact]
+        public async Task CreateTransactionAsync_ShouldNotCreate()
+        {
+            var bankProfit = 1;
+            var amount = 10;
+            var fromAccountId = 1;
+            var toAccountId = 1;
+            var currency = "GEL";
+            var isATM = true;
+
+            _mockUnitOfWork.Setup(u => u.TransactionDetailsRepository.CreateTransactionAsync(It.IsAny<TransactionDetails>())).ReturnsAsync(0);
+
+            var response = await _transactionDetailsService.CreateTransactionAsync(bankProfit, amount, fromAccountId, toAccountId, currency);
+
+            Assert.False(response.Success);
+            Assert.Equal("Transaction could not be created, something happened!", response.Message);
+            Assert.Equal(400, response.StatusCode);
+
+            _mockUnitOfWork.Verify(u => u.SaveChanges(), Times.Never());
+            _mockUnitOfWork.Verify(u => u.TransactionDetailsRepository, Times.Exactly(2));
+        }
+
+
+        [Fact]
+        public async Task NumberOfTransactionsChartAsync_ShouldReturnValue()
+        {
+            var transactionCountChartDtos = new List<TransactionCountChartDTO>();
+            _mockUnitOfWork.Setup(u => u.TransactionDetailsRepository.NumberOfTransactionsLastMonthAsync()).ReturnsAsync(transactionCountChartDtos);
+
+            var response = await _transactionDetailsService.NumberOfTransactionsChartAsync();
+
+            Assert.True(response.Success);
+            Assert.Equal("Transaction Count retreived!", response.Message);
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal(transactionCountChartDtos, response.Data);
+
+            _mockUnitOfWork.Verify(u => u.TransactionDetailsRepository, Times.Once());
+        }
+
+
+        [Fact]
+        public async Task NumberOfTransactionsChartAsync_ShouldNotReturnValue()
+        {
+            List<TransactionCountChartDTO> transactionCountChartDtos = null;
+            _mockUnitOfWork.Setup(u => u.TransactionDetailsRepository.NumberOfTransactionsLastMonthAsync()).ReturnsAsync(transactionCountChartDtos);
+
+            var response = await _transactionDetailsService.NumberOfTransactionsChartAsync();
+
+            Assert.False(response.Success);
+            Assert.Equal("Transaction Count couldn't be retreived!", response.Message);
+            Assert.Equal(404, response.StatusCode);
+            Assert.Null(response.Data);
+
+            _mockUnitOfWork.Verify(u => u.TransactionDetailsRepository, Times.Once());
+        }
+
+
+        [Fact]
+        public async Task BankProfitByTimePeriodAsync_ShouldReturnValue()
+        {
+            var bankProfitDtos = new List<BankProfitDTO>();
+            _mockUnitOfWork.Setup(u => u.TransactionDetailsRepository.GetBankProfitByTimePeriodAsync()).ReturnsAsync(bankProfitDtos);
+
+            var response = await _transactionDetailsService.BankProfitByTimePeriodAsync();
+
+            Assert.True(response.Success);
+            Assert.Equal("Bank profit retrieved successfully.", response.Message);
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal(bankProfitDtos, response.Data);
+
+            _mockUnitOfWork.Verify(u => u.TransactionDetailsRepository, Times.Once());
+        }
+
+
+        [Fact]
+        public async Task BankProfitByTimePeriodAsync_ShouldNotReturnValue()
+        {
+            List<BankProfitDTO> bankProfitDtos = null;
+            _mockUnitOfWork.Setup(u => u.TransactionDetailsRepository.GetBankProfitByTimePeriodAsync()).ReturnsAsync(bankProfitDtos);
+
+            var response = await _transactionDetailsService.BankProfitByTimePeriodAsync();
+
+            Assert.False(response.Success);
+            Assert.Equal("No bank profit data found.", response.Message);
+            Assert.Equal(404, response.StatusCode);
+            Assert.Null(response.Data);
+
+            _mockUnitOfWork.Verify(u => u.TransactionDetailsRepository, Times.Once());
+        }
+
+        [Fact]
+        public async Task TotalIncomeExpenseAsync_ShouldReturnValue()
+        {
+            var dateRangeDto = new DateRangeDTO() { FromDate = DateTime.Now.AddMonths(-1), ToDate = DateTime.Now.AddDays(-1) };
+            string email = "t@gmail.com";
+            var incomeExpense = new IncomeExpenseDTO
+            {
+                Income = new Dictionary<string, decimal> { { "GEL", 200.65m } },
+                Expense = new Dictionary<string, decimal> { { "GEL", 121.65m } }
+            };
+
+            _mockUnitOfWork.Setup(u => u.TransactionDetailsRepository.GetTotalIncomeAsync(dateRangeDto.FromDate, dateRangeDto.ToDate, email)).ReturnsAsync(incomeExpense.Income);
+            _mockUnitOfWork.Setup(u => u.TransactionDetailsRepository.GetTotalExpenseAsync(dateRangeDto.FromDate, dateRangeDto.ToDate, email)).ReturnsAsync(incomeExpense.Expense);
+
+            var response = await _transactionDetailsService.TotalIncomeExpenseAsync(dateRangeDto,email);
+
+            Assert.True(response.Success);
+            Assert.Equal("Income and Expense retreived!", response.Message);
+            Assert.Equal(200, response.StatusCode);
+            Assert.Equal(incomeExpense.Income, response.Data.Income);
+            Assert.Equal(incomeExpense.Expense, response.Data.Expense);
+
+            _mockUnitOfWork.Verify(u => u.TransactionDetailsRepository, Times.Exactly(2));
+        }
+
+        [Fact]
+        public async Task TotalIncomeExpenseAsync_ShouldNotReturnValue()
+        {
+            var dateRangeDto = new DateRangeDTO() { FromDate = DateTime.Now.AddDays(-1), ToDate = DateTime.Now.AddMonths(-1) };
+            string email = "t@gmail.com";
+
+            var response = await _transactionDetailsService.TotalIncomeExpenseAsync(dateRangeDto, email);
+
+            Assert.False(response.Success);
+            Assert.Equal("Provide correct time, toDate cannot be yearlier than fromDate!", response.Message);
+            Assert.Equal(400, response.StatusCode);
+            Assert.Null(response.Data);
+
+            _mockUnitOfWork.Verify(u => u.TransactionDetailsRepository, Times.Never());
+        }
+
+        [Fact]
+        public async Task CalculateATMWithdrawalTransactionAsync_ShouldReturnValue()
+        {
+            string cardNumber = "4998892941729115";
+            string pin = "1234";
+            decimal withdrawalAmount = 200;
+            string withdrawalCurrency = "GEL";
+            var accountInfo = new BalanceAndWithdrawalDTO() { Amount = 1200, Currency = "USD", WithdrawnAmountIn24Hours = 200 };
+
+            _mockUnitOfWork.Setup(u => u.CardRepository.GetBalanceAndWithdrawnAmountAsync(cardNumber, pin)).ReturnsAsync(accountInfo);
+            _mockExchangeRateService.Setup(er => er.GetCurrencyRateAsync(withdrawalCurrency, accountInfo.Currency)).ReturnsAsync(0.333m);
+
+            Mock<IConfigurationSection> mockSectionATMPercent = new Mock<IConfigurationSection>();
+            mockSectionATMPercent.Setup(x => x.Value).Returns("2");
+
+           
+            _mockConfiguration.Setup(x => x.GetSection("TransactionFees:AtmWithdrawalPercent")).Returns(mockSectionATMPercent.Object);
+
+            Mock<IConfigurationSection> mockSectionATMLimit = new Mock<IConfigurationSection>();
+            mockSectionATMLimit.Setup(x => x.Value).Returns("10000");
+
+           
+            _mockConfiguration.Setup(x => x.GetSection("TransactionFees:AtmWithdrawalLimitForDay")).Returns(mockSectionATMLimit.Object);
+
+            var response = await _transactionDetailsService.CalculateATMWithdrawalTransactionAsync(cardNumber, pin, withdrawalAmount, withdrawalCurrency);      
+
+
+            Assert.True(response.Success);
+            Assert.Equal(200, response.StatusCode);
+            Assert.NotNull(response.Data);
+
+            _mockUnitOfWork.Verify(u => u.CardRepository, Times.Once());
+        }
         [Fact]
         public async Task AverageBankProfitAsync_ShouldNotReturnAverageBankProfit()
         {
--- BankingSystem.UnitTests/TransactionOperationServiceTests.cs
+++ BankingSystem.UnitTests/TransactionOperationServiceTests.cs
@@ -1,5 +1,8 @@
 ﻿using BankingSystem.Application.FacadeServices;
+using BankingSystem.Contracts.DTOs.UserBanking;
 using BankingSystem.Contracts.Interfaces.IServices;
+using BankingSystem.Contracts.Response;
+using BankingSystem.Domain.Entities;
 using Moq;
 
 namespace BankingSystem.UnitTests
@@ -18,6 +21,78 @@ namespace BankingSystem.UnitTests
             _mockTransactionDetailsService = new Mock<ITransactionDetailsService>();
 
             _transactionOperationService = new TransactionOperationService(_mockAccountService.Object, _mockCardService.Object, _mockTransactionDetailsService.Object);
+        }
+
+        [Fact]
+        public async Task OnlineTransactionAsync_ShouldTransferFromAccountToAccount()
+        {
+            var createTransactionDto = new CreateOnlineTransactionDTO()
+            {
+                Amount = 20,
+                FromIBAN = "GE38CD9670132279404959",
+                ToIBAN = "GE38CD9670132279404958"
+            };
+            string email = "t@gmail.com";
+            bool isSelfTransfer = false;
+
+            var transferAccounts = new TransferAccountsDTO() { From = new Account() { Amount = 500}, To = new Account() };
+            var validateAccountsResponse = new Response<TransferAccountsDTO>().Set(true, "Accounts validated!", transferAccounts, 200);
+            _mockAccountService.Setup(a => a.ValidateAccountsForOnlineTransferAsync(createTransactionDto.FromIBAN,
+                createTransactionDto.ToIBAN, email, isSelfTransfer)).ReturnsAsync(validateAccountsResponse);
+
+            var transferAmounts = new TransferAmountCalculationDTO() { AmountFromAccount = 22, AmountToAccount = 20, BankProfit = 2 };
+            var calculateTransferAmountResponse = new Response<TransferAmountCalculationDTO>().Set(true, "", transferAmounts, 200);
+            _mockTransactionDetailsService.Setup(td => td.CalculateTransferAmountAsync(It.IsAny<string>(), It.IsAny<string>(),
+                It.IsAny<decimal>(), It.IsAny<bool>())).ReturnsAsync(calculateTransferAmountResponse);
+
+            var updateAccountsResponse = new SimpleResponse().Set(true, "Balance updated successfully!", 200);
+            _mockAccountService.Setup(a => a.UpdateAccountsAmountAsync(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<decimal>(), It.IsAny<decimal>()))
+            .ReturnsAsync(updateAccountsResponse);
+
+            var createTransactionResponse = new SimpleResponse().Set(true, "Transaction was successfull!", 200);
+            _mockTransactionDetailsService.Setup(td => td.CreateTransactionAsync(It.IsAny<decimal>(), It.IsAny<decimal>(), It.IsAny<int>(), 
+                It.IsAny<int>(), It.IsAny<string>(), false)).ReturnsAsync(createTransactionResponse);
+
+            var response = await _transactionOperationService.OnlineTransactionAsync(createTransactionDto, email, isSelfTransfer);
+
+            Assert.True(response.Success);
+            Assert.Equal("Transaction was successfull!", response.Message);
+            Assert.Equal(200, response.StatusCode);
+
+            _mockAccountService.VerifyAll();
+
+        }
+        [Fact]
+        public async Task OnlineTransactionAsync_ShouldNotTransferNotEnoughMoney()
+        {
+            var createTransactionDto = new CreateOnlineTransactionDTO()
+            {
+                Amount = 20,
+                FromIBAN = "GE38CD9670132279404959",
+                ToIBAN = "GE38CD9670132279404958"
+            };
+            string email = "t@gmail.com";
+            bool isSelfTransfer = false;
+
+            var transferAccounts = new TransferAccountsDTO() { From = new Account() { Amount = 20.5m }, To = new Account() };
+            var validateAccountsResponse = new Response<TransferAccountsDTO>().Set(true, "Accounts validated!", transferAccounts, 200);
+            _mockAccountService.Setup(a => a.ValidateAccountsForOnlineTransferAsync(createTransactionDto.FromIBAN,
+                createTransactionDto.ToIBAN, email, isSelfTransfer)).ReturnsAsync(validateAccountsResponse);
+
+            var transferAmounts = new TransferAmountCalculationDTO() { AmountFromAccount = 22, AmountToAccount = 20, BankProfit = 2 };
+            var calculateTransferAmountResponse = new Response<TransferAmountCalculationDTO>().Set(true, "", transferAmounts, 200);
+            _mockTransactionDetailsService.Setup(td => td.CalculateTransferAmountAsync(It.IsAny<string>(), It.IsAny<string>(),
+                It.IsAny<decimal>(), It.IsAny<bool>())).ReturnsAsync(calculateTransferAmountResponse);
+
+
+            var response = await _transactionOperationService.OnlineTransactionAsync(createTransactionDto, email, isSelfTransfer);
+
+            Assert.False(response.Success);
+            Assert.Equal("You don't have enough money to transfer on your account!", response.Message);
+            Assert.Equal(400, response.StatusCode);
+
+            _mockAccountService.VerifyAll();
+
         }
     }
 }
