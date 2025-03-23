982b8bf59d01f36627a3c31cf855d3991f449aa1
shamu <shamugiatamar22@gmail.com>
2025-03-23 13:09:48 +0400

    added transaction operation service test

---
+++ BankingSystem.UnitTests/TransactionOperationServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using BankingSystem.Application.FacadeServices;
+using BankingSystem.Contracts.Interfaces.IServices;
+using Moq;
+
+namespace BankingSystem.UnitTests
+{
+    public class TransactionOperationServiceTests
+    {
+        private readonly Mock<IAccountService> _mockAccountService;
+        private readonly Mock<ICardService> _mockCardService;
+        private readonly Mock<ITransactionDetailsService> _mockTransactionDetailsService;
+        private readonly TransactionOperationService _transactionOperationService;
+
+        public TransactionOperationServiceTests()
+        {
+            _mockAccountService = new Mock<IAccountService>();
+            _mockCardService = new Mock<ICardService>();
+            _mockTransactionDetailsService = new Mock<ITransactionDetailsService>();
+
+            _transactionOperationService = new TransactionOperationService(_mockAccountService.Object, _mockCardService.Object, _mockTransactionDetailsService.Object);
+        }
+    }
+}
